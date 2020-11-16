af134e8a0738a3fc1d69d11f7e69ffaeba2cb44e
Batyr0111 <63508792+Batyr0111@users.noreply.github.com>
2020-11-16 18:07:55 +0600

    46

---
+++ db/schedule17.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 16 2020 г., 14:28
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `schedule17`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `classroom`
+--
+
+CREATE TABLE `classroom` (
+  `classroom_id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `day`
+--
+
+CREATE TABLE `day` (
+  `day_id` tinyint(4) NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `day`
+--
+
+INSERT INTO `day` (`day_id`, `name`) VALUES
+(1, 'Понедельник'),
+(2, 'Вторник'),
+(3, 'Среда'),
+(4, 'Четверг'),
+(5, 'Пятница'),
+(6, 'Суббота');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gender`
+--
+
+CREATE TABLE `gender` (
+  `gender_id` tinyint(4) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `gender`
+--
+
+INSERT INTO `gender` (`gender_id`, `name`) VALUES
+(1, 'Мужской'),
+(2, 'Женский');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gruppa`
+--
+
+CREATE TABLE `gruppa` (
+  `gruppa_id` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `special_id` int(11) NOT NULL,
+  `date_begin` date NOT NULL,
+  `date_end` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_num`
+--
+
+CREATE TABLE `lesson_num` (
+  `lesson_num_id` int(11) NOT NULL,
+  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time_lesson` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `lesson_num`
+--
+
+INSERT INTO `lesson_num` (`lesson_num_id`, `name`, `time_lesson`) VALUES
+(1, '1 пара', '08:30:00'),
+(2, '2 пара', '10:10:00'),
+(3, '3 пара', '12:20:00'),
+(4, '4 пара', '14:00:00'),
+(5, '5 пара', '15:40:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lesson_plan`
+--
+
+CREATE TABLE `lesson_plan` (
+  `lesson_plan_id` int(11) NOT NULL,
+  `gruppa_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `otdel`
+--
+
+CREATE TABLE `otdel` (
+  `otdel_id` smallint(6) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `otdel`
+--
+
+INSERT INTO `otdel` (`otdel_id`, `name`, `active`) VALUES
+(1, 'Программирование', 1),
+(2, 'Общеобразовательные дисциплины', 1),
+(3, 'Строительство', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `lesson_plan_id` int(11) NOT NULL,
+  `day_id` tinyint(4) NOT NULL,
+  `lesson_num_id` int(11) NOT NULL,
+  `classroom_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `special`
+--
+
+CREATE TABLE `special` (
+  `special_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `otdel_id` smallint(6) NOT NULL,
+  `active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `special`
+--
+
+INSERT INTO `special` (`special_id`, `name`, `otdel_id`, `active`) VALUES
+(1, 'Информационные система', 1, 1),
+(2, 'Нефтегазовое дело', 2, 1),
+(3, 'Строительство и эксплуатация зданий и сооружений', 3, 1),
+(4, 'Электроснабжение', 3, 1),
+(5, 'Вычислительная техника и программное обеспечение', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `student`
+--
+
+CREATE TABLE `student` (
+  `user_id` bigint(20) NOT NULL,
+  `gruppa_id` int(11) NOT NULL,
+  `num_zach` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `otdel_id` int(11) NOT NULL,
+  `hours` smallint(6) NOT NULL,
+  `active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `user_id` bigint(20) NOT NULL,
+  `otdel_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL,
+  `lastname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expired_at` int(11) DEFAULT NULL,
+  `gender_id` tinyint(4) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `classroom`
+--
+ALTER TABLE `classroom`
+  ADD PRIMARY KEY (`classroom_id`);
+
+--
+-- Индексы таблицы `day`
+--
+ALTER TABLE `day`
+  ADD PRIMARY KEY (`day_id`);
+
+--
+-- Индексы таблицы `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`gender_id`);
+
+--
+-- Индексы таблицы `gruppa`
+--
+ALTER TABLE `gruppa`
+  ADD PRIMARY KEY (`gruppa_id`);
+
+--
+-- Индексы таблицы `lesson_num`
+--
+ALTER TABLE `lesson_num`
+  ADD PRIMARY KEY (`lesson_num_id`);
+
+--
+-- Индексы таблицы `lesson_plan`
+--
+ALTER TABLE `lesson_plan`
+  ADD PRIMARY KEY (`lesson_plan_id`);
+
+--
+-- Индексы таблицы `otdel`
+--
+ALTER TABLE `otdel`
+  ADD PRIMARY KEY (`otdel_id`);
+
+--
+-- Индексы таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`schedule_id`);
+
+--
+-- Индексы таблицы `special`
+--
+ALTER TABLE `special`
+  ADD PRIMARY KEY (`special_id`);
+
+--
+-- Индексы таблицы `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Индексы таблицы `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`);
+
+--
+-- Индексы таблицы `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
